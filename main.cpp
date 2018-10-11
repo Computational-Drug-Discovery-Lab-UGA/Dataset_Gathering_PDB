35e216dcfaaacd0a35643379d1938ec3867ddf53
Jack Nadaud <jmn07305@uga.edu>
2018-10-11 15:03:11 -0400

    added pdb file searcher

--- main.cpp
+++ main.cpp
@@ -3,9 +3,30 @@
 //
 
 
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
-int main(int argc, char* argv[]){
-    cout<<"hello world"<<std::endl;
+
+int main() {
+    string filename = "1pru(1).txt";
+    // check that file was opened
+    ifstream file("1pru(1).txt");
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file!\n";
+        return -1;
+    }
+    //search keyword
+    const string keyword = "DNA BINDING";
+
+    //read loop
+    string PDBline;
+    while (getline(file, PDBline)) {
+        if (PDBline.find(keyword) != string::npos) {
+            cout << filename << "\n";
+            // Print name of file
+
+        }
+    }
 }
