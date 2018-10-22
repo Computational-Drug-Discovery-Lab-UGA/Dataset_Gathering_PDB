8469d5cef267db3f74623682fc47ee36305c927b
Jack Nadaud <jmn07305@uga.edu>
2018-10-22 14:20:36 -0400

    few changes and two searches

--- main.cpp
+++ main.cpp
@@ -1,29 +1,29 @@
-
-
 #include <fstream>
 #include <iostream>
 #include <string>
 
-using namespace std;
 
-int main() {
-    string filename = "../pdbs/1arf.txt";
-    // check that file was opened
-    ifstream file("../pdbs/1arf.txt");
+int main(int argc, char* argv[]) {
+    std::string filename = "../pdbs/1pru(1).txt";
+    std::ifstream file("../pdbs/1pru(1).txt");
     if (!file.is_open()) {
         std::cerr << "Failed to open file!\n";
         return -1;
     }
-    //search keyword
-    const string keyword = "DNA BINDING";
 
-    //read loop
-    string PDBline;
-    while (getline(file, PDBline)) {
-        if (PDBline.find(keyword) != string::npos) {
-            cout << filename << "\n";
-            // Print name of file
-
+    std::string PDB;
+    const std::string keyword1="DNA-BINDING";
+    while (getline(file, PDB)) {
+        if (PDB.find(keyword1) != std::string::npos) {
+            std::cout <<"files containing 'DNA-BINDING' include: " <<filename << "\n";
+            break;
+        }
+    }
+    const std::string keyword2="RNA-BINDING";
+    while (getline(file, PDB)) {
+        if (PDB.find(keyword2) != std::string::npos) {
+            std::cout << "files containing 'RNA-BINDING' include: " << filename << "\n";
+            break;
         }
     }
 }
