ffa2fb9764ef734f9576770a3edb4ee2a0fdd18d
Jack Nadaud <jmn07305@uga.edu>
2018-11-15 16:21:05 -0500

    void parsedirectory function working now

--- main.cpp
+++ main.cpp
@@ -9,64 +9,65 @@
 #include <vector>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <algorithm>
+#include <iterator>
 
-
-//directory of sample pdb folder I used is /Users/jacknadaud/Desktop/324pdb
+//directory of sample pdb folder /Users/jacknadaud/Desktop/324pdb
 
 void parseDirectory(std::string dirPath, std::vector<std::string> &files) {
-}
-int main(int argc, char *argv[]) {
-    std::vector<std::string> files;
-    std::string dirPath = "/Users/jacknadaud/Desktop/324pdb";
-
     DIR *dir;
-    struct dirent *in_file;
-    if ((dir = opendir("/Users/jacknadaud/Desktop/324pdb")) != nullptr) {
-
-        //std::string newdirectoryname;
-        //mkdir( newdirectoryname.c_str())
-
-        while ((in_file = readdir(dir)) != nullptr) {
-            std::string currentFileName = in_file->d_name;
-            if (currentFileName.find(".pdb") == std::string::npos &&
-                currentFileName.find(".json") == std::string::npos)
-                continue; //do we have any .json files or a reason to keep extension?
-            currentFileName = dirPath + "/" + currentFileName;
-            //files.push_back(currentFileName);
-
-            std::cout<< currentFileName<< "\n";
-            std::ifstream file(currentFileName);
-            if (!file.is_open()) {
-                std::cerr << "Failed to open file!\n";
-                return -1;
-            }
-
-            const std::string keyword1 = "DNA";
-            while (getline(file, currentFileName)) {
-                if (currentFileName.find(keyword1) != std::string::npos) {
-                    std::cout << "files containing 'DNA' include: " << currentFileName << "\n";
-                    break;
-                }
-                /*/
-                int numFiles = (int) files.size();
-                std::string currentFile = "";
-                for (int i = 0; i < numFiles; ++i) {
-                    currentFile = files[i];}
-                //*/
-
-                /*/
-                std::string currentFile = dirPath + "/" + files[i];
-                std::string currentFileName = newDirPath + "/" +files[i];
-                std::ifstream src(fileName.c_str(), std::ios::binary);
-                std::ofstream dest(currentFileName.c_str(), std::ios::binary);
-                dest<<src.rdbuf();
-                //*/
-            }
-            closedir(dir);
-        }
-    } else {
-        perror("");
-        return EXIT_FAILURE;
+    if (nullptr == (dir = opendir(dirPath.c_str()))){
+        printf("Error : Failed to open input directory %s\n",dirPath.c_str());
+        exit(-1);
     }
-
+    struct dirent* in_file;
+    while((in_file = readdir(dir)) != nullptr){
+        std::string currentFileName = in_file->d_name;
+        if(currentFileName.find(".pdb") == std::string::npos &&
+           currentFileName.find(".json") == std::string::npos) continue;
+        currentFileName = dirPath + "/" + currentFileName;
+        files.push_back(currentFileName);
+    }
+    closedir(dir);
 }
+
+int main(int argc, char *argv[]) {
+
+    std::vector<std::string> files;
+    std::string directory = "/Users/jacknadaud/Desktop/324pdb";
+    parseDirectory(directory, files);
+    //while () {
+        int numFiles = (int) files.size();
+        std::string currentFile = "";
+        for (int i = 0; i < numFiles; ++i) {
+            currentFile = files[i];
+        }
+        std::ifstream file(currentFile);
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file!\n";
+            //return -1;
+        }
+        const std::string keyword1 = "DNA";
+        while (getline(file, currentFile)) {
+            if (currentFile.find(keyword1) != std::string::npos) {
+                std::cout << "files containing 'DNA' include: " << currentFile << "\n";
+                break;
+            }
+        }
+    }
+//}
+
+
+
+
+
+
+/*/
+        std::string currentFile = dirPath + "/" + files[i];
+        std::string currentFileName = newDirPath + "/" +files[i];
+        std::ifstream src(fileName.c_str(), std::ios::binary);
+        std::ofstream dest(currentFileName.c_str(), std::ios::binary);
+        dest<<src.rdbuf();
+
+
+        //*/
