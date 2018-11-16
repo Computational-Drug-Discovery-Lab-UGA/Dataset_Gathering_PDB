15db0ec907f0d389763dfa827f9f4b4f8b52976d
Jack Nadaud <jmn07305@uga.edu>
2018-11-15 22:20:53 -0500

    Working, read comments

--- main.cpp
+++ main.cpp
@@ -12,8 +12,35 @@
 #include <algorithm>
 #include <iterator>
 
-//directory of sample pdb folder /Users/jacknadaud/Desktop/324pdb
+//directory of sample pdb folder /Users/jacknadaud/Desktop/PDBS/324pdb
+//created a new directory with address /Users/jacknadaud/Desktop/PDBS/DNAPDB for new PDBS
 
+void replaceAll(std::string& source, const std::string& from, const std::string& to) {
+    std::string newString;
+    newString.reserve(source.length());  // avoids a few memory allocations
+
+    std::string::size_type lastPos = 0;
+    std::string::size_type findPos;
+
+    while(std::string::npos != (findPos = source.find(from, lastPos)))
+    {
+        newString.append(source, lastPos, findPos - lastPos);
+        newString += to;
+        lastPos = findPos + from.length();
+    }
+
+    // Care for the rest after last occurrence
+    newString += source.substr(lastPos);
+
+    source.swap(newString);
+}
+
+bool copyFile(std::string SRC, std::string DEST) {
+    std::ifstream src(SRC, std::ios::binary);
+    std::ofstream dest(DEST, std::ios::binary);
+    dest << src.rdbuf();
+    return src && dest;
+}
 void parseDirectory(std::string dirPath, std::vector<std::string> &files) {
     DIR *dir;
     if (nullptr == (dir = opendir(dirPath.c_str()))){
@@ -33,11 +60,8 @@ void parseDirectory(std::string dirPath, std::vector<std::string> &files) {
 
 int main(int argc, char *argv[]) {
 
-    //std::string PDBwithDNA;
-    //mkdir(PDBwithDNA.c_str());
-
     std::vector<std::string> files;
-    std::string directory = "/Users/jacknadaud/Desktop/324pdb";
+    std::string directory = "/Users/jacknadaud/Desktop/PDBS/324pdb";
     parseDirectory(directory, files);
 
         int numFiles = (int) files.size();
@@ -52,22 +76,41 @@ int main(int argc, char *argv[]) {
             std::string pdb;
             while (getline(file, pdb)) {
                 if (pdb.find(keyword1) != std::string::npos) {
-                    std::cout << "files containing 'DNA' include: " << currentFile << "\n";
+
+                    //std::cout << "files containing 'DNA' include: " << currentFile << "\n";
+
+                    std::string newDirPath = "/Users/jacknadaud/Desktop/PDBS/DNAPDB";
+                    copyFile(directory, newDirPath);
+
+                    return copyFile(argv[1], argv[2]) ? 0 : 1;
+
+
+
+
+                    /*/  method below uses void replaceAll and changes filename
+                         puts in new folder but new files are just empty pdbs
+
+                     std::string newDirPath = "/Users/jacknadaud/Desktop/PDBS/DNAPDB";
+                    replaceAll(currentFile, std::string("324pdb"), std::string("DNAPDB"));
+                    std::ifstream src(currentFile.c_str(), std::ios::binary);
+                    std::ofstream dest(currentFile.c_str(), std::ios::binary);
+                    dest<<src.rdbuf();
 
                     /*/
-                    std::string currentFile = dirPath + "/" + files[i];
-                    std::string currentFileName = newDirPath + "/" +files[i];
-                    std::ifstream src(fileName.c_str(), std::ios::binary);
-                    std::ofstream dest(currentFileName.c_str(), std::ios::binary);
-                    dest<<src.rdbuf();
-                    //*/
 
-                    break;
+                    /*/  method below similar to one in code, can't figure out
+
+                    std::string currentFile = directory + "/" + files[i];
+                    std::string newFileName = newDirPath + "/" + files[i];
+                    std::cout << newFileName <<"\n";
+                    std::ifstream src(currentFile.c_str(), std::ios::binary);
+                    std::ofstream dest(newFileName.c_str(), std::ios::binary);
+                    dest<<src.rdbuf();
+                    /*/
                 }
             }
         }
     }
-//}
 
 
 
